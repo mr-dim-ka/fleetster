42fd0d6e4dcfe29e199d73dee23bb2fff798e78d
mr-dim-ka <kalinin9.7@mail.ru>
2022-03-24 14:32:34 +0600

    adaptive section slider

--- app/js/main.js
+++ app/js/main.js
@@ -8,7 +8,21 @@ $(function () {
 		infinite: true,
 		slidesToShow: 3,
 		slidesToScroll: 1,
-		adaptiveHeight: true
+		adaptiveHeight: true,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 640,
+				settings: {
+					slidesToShow: 1
+				}
+			}
+		]
 	});
 
 	$('.header__burger, .menu a').on('click', function () {
